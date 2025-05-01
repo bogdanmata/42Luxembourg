845f44c5e6a46a51a16f5b5e6f2702718c986f6c
bogdanmata <bogdanmata@gmail.com>
2025-05-01 13:12:38 +0200

    correction to change the year from 2025 to 2024

--- shell00/ex02/run.sh
+++ shell00/ex02/run.sh
@@ -5,29 +5,29 @@ rm -f exo2.tar
 
 mkdir test0
 chmod 715 test0
-touch -t 202506012047 test0
+touch -t 202406012047 test0
 
 dd if=/dev/zero of=test1 bs=1 count=4
 chmod 714 test1
-touch -t 202506012146 test1
+touch -t 202406012146 test1
 
 mkdir test2
 chmod 504 test2
-touch -t 202506012245 test2
+touch -t 202406012245 test2
 
 dd if=/dev/zero of=test3 bs=1 count=1
 chmod 404 test3
-touch -t 202506012344 test3
+touch -t 202406012344 test3
 
 dd if=/dev/zero of=test4 bs=1 count=2
 chmod 641 test4
-touch -t 202506012343 test4
+touch -t 202406012343 test4
 
 ln test3 test5
-touch -t 202506012344 test5
+touch -t 202406012344 test5
 
 ln -s test0 test6
-touch -h -t 202506012220 test6
+touch -h -t 202406012220 test6
 
 ls -l
 

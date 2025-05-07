4d7ff1be1e956d27dbf814d1bba9d90eb75b2a95
Bogdan Mata <admin@bogdanmata.org>
2025-05-07 17:33:07 +0200

    c01 ex03 (with malloc)

--- c01/ex03/ft_div_mod.c
+++ c01/ex03/ft_div_mod.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
+#include <malloc.h>
 
 void ft_div_mod(int a, int b, int *div, int *mod);
 
 int main() {
     // values
     int a = 16, b = 3;
-    int div, mod;
-    ft_div_mod(a, b, &div, &mod);
+    int *div = malloc(sizeof(int)), *mod = malloc(sizeof(int));
+    ft_div_mod(a, b, div, mod);
 
-    printf("%d %d %d %d\n", a, b, div, mod);
+    printf("%d %d %d %d\n", a, b, *div, *mod);
 }
 
 void ft_div_mod(int a, int b, int *div, int *mod) {

aa025a5b10ddbf16340953bd52f3fa133bd99b50
Bogdan Mata <admin@bogdanmata.org>
2025-05-07 17:28:48 +0200

    c01 ex03

---
+++ c01/ex03/ft_div_mod.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+void ft_div_mod(int a, int b, int *div, int *mod);
+
+int main() {
+    // values
+    int a = 16, b = 3;
+    int div, mod;
+    ft_div_mod(a, b, &div, &mod);
+
+    printf("%d %d %d %d\n", a, b, div, mod);
+}
+
+void ft_div_mod(int a, int b, int *div, int *mod) {
+    (*div) = a / b;
+    (*mod) = a % b;
+}

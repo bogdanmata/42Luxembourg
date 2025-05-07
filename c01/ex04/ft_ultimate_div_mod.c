9bc68dbe5368e4ab29f1a5f9751519ce4ede7aed
Bogdan Mata <admin@bogdanmata.org>
2025-05-07 17:18:58 +0200

    c01 ex04 (expl)

--- c01/ex04/ft_ultimate_div_mod.c
+++ c01/ex04/ft_ultimate_div_mod.c
@@ -4,9 +4,13 @@
 void ft_ultimate_div_mod(int *a, int *b);
 
 int main() {
+    // values
     int a = 16, b = 3;
+    // pointers
+    int *pa = &a, *pb = &b;
     printf("%d %d\n", a, b);
-    ft_ultimate_div_mod(&a, &b);
+    // ft_ultimate_div_mod(&a, &b);
+    ft_ultimate_div_mod(pa, pb);
 
     printf("%d %d\n", a, b);
     return 0;
